e9ea4979e8969c4935adbe54edbd0af37f48cc3e
saxenabhishek <as7122000@gmail.com>
2021-03-15 01:04:17 +0530

    ✨ Question genaration route

--- Api/app.py
+++ Api/app.py
@@ -1,20 +1,61 @@
 from fastapi import FastAPI, File, UploadFile
 from src.ocr.imgtotext import paragraph as split_text, getText
 from tinydb import TinyDB, Query
+from fastapi.middleware.cors import CORSMiddleware
+from main_nlp import *
+import re
+
+# import threading
 
 app = FastAPI()
-db = TinyDB("db.json")
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
 
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...), UID: str = File(...)):
-
+    db = TinyDB("db.json")
     paragraphs = split_text(getText(file.file))
     d = {"uid": UID, "k": 0, "Alltext": []}
     for i in paragraphs:
-        d["text"].append({"text": i, "que": [], "ans": []})
+        d["Alltext"].append({"text": i, "que": [], "ans": []})
     db.insert(d)
+    doQuestions()
     return {"response": "200"}
+
+
+# async def doQuestions(uid:str):
+
+#     return True
+
+
+@app.get("/question/")
+async def sendquestions(UID: str):
+    db = TinyDB("db.json")
+    q = Query()
+    res = db.search(q.uid == UID)[0]
+    length = len(res["Alltext"])
+    if length <= res["k"]:
+        return {"state": False, "main": []}
+
+    while len(text := res["Alltext"][db.search(q.uid == UID)[0]["k"]]["text"]) < 10:
+        db.update({"k": res["k"] + 1}, q.uid == UID)
+
+    print(text)
+    text = re.sub("/s+", " ", text)
+    print(text)
+    db.update({"k": res["k"] + 1}, q.uid == UID)
+
+    print(que_ans := question_answers(text))
+
+    return {"state": False, "main": [*que_ans]}
 
 
 @app.get("/")
--- main_nlp.py
+++ main_nlp.py
@@ -2,10 +2,11 @@ from src.transformerswrapper import CustomTransformer
 
 customtrans = CustomTransformer()
 
-def question_answers(text : str):
-    '''
+
+def question_answers(text: str):
+    """
     This function returns a list with dictionary which has questions and answers
-    '''
+    """
     main_dict = {}
 
     questionList = []
@@ -14,12 +15,12 @@ def question_answers(text : str):
     return contents
 
 
-def checkanswers(userlist : list, originallist : list, metric: int):
+def checkanswers(userlist: list, originallist: list, metric: int):
 
-    '''
+    """
     returns a boolenn value 
 
-    '''
+    """
 
     uservec = []
     originalvec = []
@@ -32,47 +33,43 @@ def checkanswers(userlist : list, originallist : list, metric: int):
 
     for i in range(len(uservec)):
         distance = customtrans.distance(uservec[i], originalvec[i])
-        
+
         if distance > metric:
             boollist.append(False)
 
         else:
             boollist.append(True)
-        
+
         for i in boollist:
             if i == True:
                 count += 1
 
-        if count < len(boollist)/2:
+        if count < len(boollist) / 2:
             return False
-        
+
     return True
 
 
+def simpler_question_answers(text: str, max_length: int, min_length: int):
 
-def simpler_question_answers(text: str , max_length :int, min_length : int):
-
-    '''
+    """
     simplifies the paragraph and generates questions and answer on the simplified text
-    '''
-
+    """
 
     easytext = customtrans.summerizer(text, max_length=max_length, min_length=min_length)
     return question_answers(easytext)
 
-if __name__ == '__main__':
 
-    text1 = '''
+if __name__ == "__main__":
+
+    text1 = """
         The physical nature of time is addressed by general relativity with respect to events in space-time.
         Examples of events are the collision or two particles, the explosion of a supernova, or the arrival of a rocket ship. 
         Every event can be assigned four numbers representing its time and position (the event's coordinates). 
         However, the numerical values are different for different observers. 
-        '''
+        """
 
-    text2 = '''
-        The physical nature of time is addressed by general relativity with respect to events in space-time.
-        Examples of events are the collision or two particles, the explosion of a supernova, or the arrival of a rocket ship. 
-        Every event can be assigned four numbers representing its time and position (the event's coordinates). 
-        However, the numerical values are different for different observers. 
-
-        '''         
+    # text2 = """Refraction is the bending of a wave when it enters a medium where its speed is different. The refraction of light when it passes from a fast medium to a slow medium bends the light ray toward the normal to the boundary between the two media. Refraction is responsible for image formation by lenses and the eye."""
+    # text2 = "1. INTRODUCTION"
+    print(text1)
+    print(question_answers(text1))
